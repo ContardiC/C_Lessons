aadedccf6e20255ebd36e2f170587ca76fe2b62f
Carlo Contardi <c.contardi@icloud.com>
2022-11-03 09:22:53 +0100

    conta in due versi added

--- Cicli e selezione/conta_in_due_sensi.c
+++ Cicli e selezione/conta_in_due_sensi.c
@@ -1,18 +1,23 @@
 #include<stdio.h>
 int main(){
     int i, j, n;
-    /* 
+ 
     do{
         printf("inserisci il lato del quadrato\n");
         scanf("%d",&n);
     }while(n<=0);
     int tmp=1;
-    for(i=1;i<=n*n;i++){
-        printf("%d \n",i);
-        for(j=i+n;j>=n;j--){
-            printf("%d",j);
+    for(i=1;i<=(n*n);i++){
+        printf("%d \t",i);
+        if(i%n == 0){
+            printf("\n");
+            for(j=n+i;j>i;j--){
+                printf("%d\t",j);
+            }
+            i=j+n;
+            printf("\n");
         }
     }
     return 0;
-    */
+    
 }
