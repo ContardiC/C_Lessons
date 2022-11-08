a9b75a2361fe9b5bf1de84368e903a6f98829844
ContardiC <c.contardi@icloud.com>
2022-11-08 17:28:14 +0100

    alfabeto added

--- ASCII/ascii.c
+++ ASCII/ascii.c
@@ -1,9 +1,14 @@
 #include<stdio.h>
 int main(){
     char c;
-    c=65;
-    int d=65;
-    printf("Al codice ASCII %d corrisponde il carattere %c\n",c,c);
-    printf("Al codice ASCII %d corrisponde il carattere %c\n",d,d);
+    c=97;
+    char d='a';
+    for(c=97;c<=122;c++){
+        printf("%c \t",c);
+    }
+    printf("\n\n\n");
+    for(d='a';d<='z';d++){
+        printf("%d \t",d);
+    } 
     return 0;
 }
