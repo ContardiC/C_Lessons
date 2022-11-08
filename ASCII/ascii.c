c202a1fd7be64a59ff6c7d4efabbb9774f0a08f3
ContardiC <c.contardi@icloud.com>
2022-11-08 16:45:15 +0100

    ascii added

---
+++ ASCII/ascii.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    char c;
+    c=65;
+    int d=65;
+    printf("Al codice ASCII %d corrisponde il carattere %c\n",c,c);
+    printf("Al codice ASCII %d corrisponde il carattere %c\n",d,d);
+    return 0;
+}
