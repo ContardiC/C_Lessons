eead312144e34e895efc633214fcbd968f3fa59c
Carlo Contardi <c.contardi@icloud.com>
2023-10-19 08:49:22 +0200

    genera numeri primi added

---
+++ cicli/genera_numeri_primi.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+    int n, i, j, primo;
+    do{
+        printf("Inserisci un numero: ");
+        scanf("%d", &n);
+    }while(n<=0);
+    int cont=0;
+    for(i=1;cont<n;i++){
+        primo = 1;
+        for(j=2; j<i; j++){
+            if(i%j==0){
+                primo = 0;
+                break;
+            }
+        }
+        if(primo==1){
+            printf("%d\n", i);
+            cont++;
+        }
+    }
+    return 0;
+}
