fd87e866c2947f8454f9a379a277ad576bd4dbcd
ContardiC <c.contardi@spacecoding.it>
2024-03-18 12:13:01 +0100

    write and read text file added

---
+++ file/write_and_read.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+struct Libro {
+    char titolo[100];
+    char autore[100];
+    int anno_pubblicazione;
+};
+
+int main() {
+    FILE *fp;
+    struct Libro libri[10];
+
+    // Apri il file in modalità scrittura
+    fp = fopen("libri.txt", "w");
+
+    // Scrivi dati nel file
+    for (int i = 0; i < 10; i++) {
+        fprintf(fp, "%s\n%s\n%d\n", libri[i].titolo, libri[i].autore, libri[i].anno_pubblicazione);
+    }
+
+    // Chiudi il file
+    fclose(fp);
+
+    // Apri il file in modalità lettura
+    FILE *fp_lettura;
+    fp_lettura = fopen("libri.txt", "r");
+
+    // Leggi dati dal file
+    while (!feof(fp_lettura)) {
+        char titolo[100], autore[100];
+        int anno_pubblicazione;
+        fscanf(fp_lettura, "%[^\n]\n%[^\n]\n%d\n", titolo, autore, &anno_pubblicazione);
+        // Usa i dati letti come desideri
+    }
+
+    // Chiudi il file di lettura
+    fclose(fp_lettura);
+
+    return 0;
+}
